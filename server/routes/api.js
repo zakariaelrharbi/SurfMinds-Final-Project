6603a8b16343242ca65478608f3768dfe898d2c5
Magueri09salah <maguerisalaheddine@gmail.com>
2024-04-15 10:35:56 +0100

    update routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,11 +11,11 @@ const router = express.Router();
 
 
 router.use('/auth', authRoute);
-router.use('/product', productRoute);
-router.use('/order', orderRoute);
-router.use('/paymentmethod', paymentMethodRoute);
-router.use('/category', categoryRoute);
-router.use('/user', userRoute);
+router.use('/products', productRoute);
+router.use('/orders', orderRoute);
+router.use('/paymentmethods', paymentMethodRoute);
+router.use('/categories', categoryRoute);
+router.use('/users', userRoute);
 
 
 
