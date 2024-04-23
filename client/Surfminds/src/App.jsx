06e773d446f1d48a437a6b1fca8927b8715c4b58
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-04-23 11:58:59 +0100

    setup front end and register page created

---
+++ client/Surfminds/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Register from '../Pages/Register'
+
+function App() {
+
+  return (
+    <>
+      <Register/>
+    </>
+  )
+}
+
+export default App
