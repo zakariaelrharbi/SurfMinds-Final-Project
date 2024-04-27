97cf0807bd6635172f2fc73c50898f814e17c2c2
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-04-27 22:04:37 +0100

    login updated

--- client/Surfminds/src/App.jsx
+++ client/Surfminds/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from '../Pages/Login/UserLogin';
 import Register from '../Pages/Register/Register';
 import Terms_and_conditions from '../Pages/Register/Terms_and_conditions';
+import UserLogin from '../Pages/Login/UserLogin';
 
 function App() {
   return (
     <div className="wrapper">
       <Routes>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<UserLogin/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/terms-and-conditions" element={<Terms_and_conditions/>}/>
       </Routes>
